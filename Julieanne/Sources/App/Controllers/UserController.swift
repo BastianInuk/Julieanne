51be2ef6309dd6427e7c70dbc169dd7415e63090
Bastian Inuk Christensen <bastian@inuk.blog>
2019-09-06 20:32:59 +0200

    Users can now be created and listed, as well as login working! https://github.com/raywenderlich/vapor-til/blob/master/Sources/App/Controllers/UsersController.swift

--- Julieanne/Sources/App/Controllers/UserController.swift
+++ Julieanne/Sources/App/Controllers/UserController.swift
@@ -6,6 +6,36 @@ final class UserController: RouteCollection {
     func boot(router: Router) throws {
         // Makes it so /api/users is the route in this controller.
         let usersRoute = router.grouped("api", "users")
+            usersRoute.get(use: getAllHandler)
+            usersRoute.get(User.parameter, use: getHandler)
         
+        /// Protected route, only for authenticated `User`s
+        let basicAuthMiddleware = User.basicAuthMiddleware(using: BCryptDigest())
+        let basicAuthGroup = usersRoute.grouped(basicAuthMiddleware)
+            basicAuthGroup.post("login", use: loginHandler)
+
+        let tokenAuthMiddleware = User.tokenAuthMiddleware()
+        let guardAuthMiddleware = User.guardAuthMiddleware()
+        let tokenAuthGroup = usersRoute.grouped(tokenAuthMiddleware, guardAuthMiddleware)
+            usersRoute.post(User.self, use: createHandler)
+    }
+    
+    func createHandler(_ req: Request, user: User) throws -> Future<User.Public> {
+      user.password = try BCrypt.hash(user.password)
+      return user.save(on: req).convertToPublic()
+    }
+
+    func getAllHandler(_ req: Request) throws -> Future<[User.Public]> {
+      return User.query(on: req).decode(data: User.Public.self).all()
+    }
+
+    func getHandler(_ req: Request) throws -> Future<User.Public> {
+      return try req.parameters.next(User.self).convertToPublic()
+    }
+
+    func loginHandler(_ req: Request) throws -> Future<Token> {
+      let user = try req.requireAuthenticated(User.self)
+      let token = try Token.generate(for: user)
+      return token.save(on: req)
     }
 }
--- Julieanne/Sources/App/Models/User.swift
+++ Julieanne/Sources/App/Models/User.swift
@@ -1,15 +1,75 @@
-import FluentSQLite
+import Foundation
 import Vapor
+import FluentPostgreSQL
+import Authentication
 
-struct User: Codable {
-    var id: Int?
+final class User: Codable {
+  var id: UUID?
+  var name: String
+  var username: String
+  var password: String
+  var email: String
+  var profilePicture: String?
+
+  init(name: String, username: String, password: String, email: String, profilePicture: String? = nil) {
+    self.name = name
+    self.username = username
+    self.password = password
+    self.email = email
+    self.profilePicture = profilePicture
+  }
+
+  final class Public: Codable {
+    var id: UUID?
+    var name: String
     var username: String
-    var mail: String
-    var password: String
+
+    init(id: UUID?, name: String, username: String) {
+      self.id = id
+      self.name = name
+      self.username = username
+    }
+  }
 }
 
-/// Makes it so `User` has a table in SQLite - For testing purpose!
-extension User: SQLiteModel {}
+extension User: PostgreSQLUUIDModel {}
+extension User: Content {}
 
-/// Allows `User` to be encoded to and decoded from HTTP messages.
-extension User: Content { }
+extension User: Migration {
+  static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
+    return Database.create(self, on: connection) { builder in
+      try addProperties(to: builder)
+      builder.unique(on: \.username)
+      builder.unique(on: \.email)
+    }
+  }
+}
+
+extension User: Parameter {}
+extension User.Public: Content {}
+
+extension User {
+  func convertToPublic() -> User.Public {
+    return User.Public(id: id, name: name, username: username)
+  }
+}
+
+extension Future where T: User {
+  func convertToPublic() -> Future<User.Public> {
+    return self.map(to: User.Public.self) { user in
+      return user.convertToPublic()
+    }
+  }
+}
+
+extension User: BasicAuthenticatable {
+  static let usernameKey: UsernameKey = \User.username
+  static let passwordKey: PasswordKey = \User.password
+}
+
+extension User: TokenAuthenticatable {    
+  typealias TokenType = Token
+}
+
+extension User: PasswordAuthenticatable {}
+extension User: SessionAuthenticatable {}
